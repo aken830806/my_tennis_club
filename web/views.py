7902da2ad6c5aae35f1ce6eb4694b52ec9063ce5
njzhan <njzhan.fcu@gmail.com>
2023-12-01 11:54:11 +0800

    feat: 新增 登入/登出 功能

---
+++ web/views.py
@@ -0,0 +1,32 @@
+from django.contrib.auth import authenticate
+from django.http import HttpResponse
+from django.shortcuts import redirect
+from django.template import loader
+from django.contrib import auth
+
+from web.forms import LoginForm
+
+
+def login(request):
+    template = loader.get_template('login.html')
+    if request.method == "POST":
+        post_form = LoginForm(request.POST)
+        if post_form.is_valid():
+            username = post_form.cleaned_data['username']
+            password = post_form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                auth.login(request, user)
+                return redirect('main')
+    else:
+        post_form = LoginForm()
+    context = {
+        'post_form': post_form,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def logout(request):
+    """登出"""
+    auth.logout(request)
+    return redirect('main')
