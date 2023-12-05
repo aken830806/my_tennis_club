3dc8a1bec05b2f3e1b3c04f60fd7cfd38ba6aa14
njzhan <njzhan.fcu@gmail.com>
2023-12-05 13:18:45 +0800

    feat: 修改 登入-新增 登入錯誤訊息

--- web/views.py
+++ web/views.py
@@ -9,6 +9,7 @@ from web.forms import LoginForm
 
 def login(request):
     template = loader.get_template('login.html')
+    message = ''
     if request.method == "POST":
         post_form = LoginForm(request.POST)
         if post_form.is_valid():
@@ -18,10 +19,12 @@ def login(request):
             if user is not None:
                 auth.login(request, user)
                 return redirect('main')
+            message = 'Login failed.'
     else:
         post_form = LoginForm()
     context = {
         'post_form': post_form,
+        'message': message,
     }
     return HttpResponse(template.render(context, request))
 
